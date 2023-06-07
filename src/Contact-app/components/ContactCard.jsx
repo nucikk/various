d1975bbf2ca0974ff062721c1a08b54315566249
nucikk <kirianuci@gmail.com>
2023-06-07 23:59:40 +0400

    fix bug

--- src/Contact-app/components/ContactCard.jsx
+++ src/Contact-app/components/ContactCard.jsx
@@ -2,16 +2,18 @@ import user from "../contact-image/user.png"
 import PropTypes from "prop-types";
 
 const ContactCard = (props) => {
-    const { name, email} = props.contact
+    const { name, emaill} = props.contact
+
   return (
     <>
     <div className="item">
       <img className="ui avatar image" src={user} alt="user" />
       <div className="content">
         <div className="header">
-            {name}
+            {name}  
         </div>
-        <div>{email}</div>
+
+        <div>{emaill}</div>
         <i className="trash alternate outline icon" style={{color: "red", marginTop: "10px"}}></i>
       </div>
       </div>
--- src/tailwind/TechStory.jsx
+++
@@ -1,9 +0,0 @@
-import "../tailwind/techno.css"
-
-const TechStory = () => {
-  return (
-    <div>TechStory</div>
-  )
-}
-
-export default TechStory
